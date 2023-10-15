6d7975600c4a7444f3a022c219d0a54130ad01b6
Kobii <harold.rockata@gmail.com>
2023-10-15 16:31:03 +0200

    Added view binding for efficiency

--- app/src/main/java/com/example/takepicture/MainActivity.kt
+++ app/src/main/java/com/example/takepicture/MainActivity.kt
@@ -4,8 +4,6 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
@@ -13,9 +11,11 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import com.example.takepicture.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
     private var capturedImageUri: Uri? = null
 
@@ -25,9 +25,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImageIntoView(uri: Uri?) {
-        val ivTakePicture = findViewById<ImageView>(R.id.ivCapturedPicture)
         uri?.let {
-            ivTakePicture.setImageURI(it)
+            binding.ivCapturedPicture.setImageURI(it)
         }
     }
 
@@ -41,7 +40,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // set up view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
             result: Boolean ->
@@ -49,8 +51,9 @@ class MainActivity : AppCompatActivity() {
                 else Log.d("TakePicture", "Something went wrong with taking the picture")
         }
 
-        val btnTakePicture = findViewById<Button>(R.id.btnTakePicture)
-        btnTakePicture.setOnClickListener { this.captureImage() }
+        binding.btnTakePicture.setOnClickListener {
+            this.captureImage()
+        }
     }
 
 }
